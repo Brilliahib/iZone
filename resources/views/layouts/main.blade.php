bcc86c4f4495bc72cc119935a486d4dedc69122e
brilliahib <brilliahib21@gmail.com>
2024-05-18 17:08:54 +0700

    delete on footer

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -19,7 +19,7 @@
 <body>
     @include('layouts.navbar')
         @yield('container')
-    @extends('layouts.footer')
+    <!-- @extends('layouts.footer') -->
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
